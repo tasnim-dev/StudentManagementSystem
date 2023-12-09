47b9be141787d844bc0285b4100763a40298733b
tasnim-dev <107361707+tasnim-dev@users.noreply.github.com>
2023-12-09 06:58:04 +0600

    create custom user

--- Project/AppManagement/admin.py
+++ Project/AppManagement/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import customUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class userModel(UserAdmin):
+    list_display =["username" , "user_type"]
+
+
+
+admin.site.register(customUser,userModel)
--- Project/AppManagement/models.py
+++ Project/AppManagement/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class customUser(AbstractUser):
+
+    USER = (
+        (1, "admin"),
+        (2, "Teacher"),
+        (3, "Student"),
+    )
+
+    user_type = models.CharField(choices=USER, max_length=50, default=1)
+    profile_img = models.ImageField(upload_to="Profile_Img")
